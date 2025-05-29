5f2badb56a4d4ad9d6ee6646b79edc4573fb6f4f
Danscotech <danakin45@gmail.com>
2025-05-29 07:10:46 +0100

    editted the task component by adding the filter by status to it

--- frontend/src/app/pages/tasks/tasks.component.ts
+++ frontend/src/app/pages/tasks/tasks.component.ts
@@ -1,16 +1,55 @@
 import { Component } from '@angular/core';
-import { Task } from '../../interfaces/task';
+import { Task, TaskStatus } from '../../interfaces/task';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  allStatus: Task[] = [
+  allStatus = [
     {
-
+      label: 'All',
+      value: 'All'
+    },
+    {
+      label: 'Open',
+      value: 'OPEN'
+    },
+    {
+      label: 'In Progress',
+      value: 'IN_PROGRESS'
+    },
+    {
+      label: 'Done',
+      value: "DONE"
     }
   ]
+
+  isLoading: boolean = false;
+  loading: boolean = false;
+  tasks: Task[] = [];
+  filteredTasks: Task[] = [];
+  statusFilter: string = 'All';
+
+  constructor(
+    private taskService: TaskService,
+  ) { }
+
+
+  filterTask(): void {
+    if (this.statusFilter === 'All') {
+      this.filteredTasks = [...this.tasks];
+    } else {
+      this.filteredTasks = this.tasks.filter(task => task.status === this.statusFilter);
+    }
+  }
 }
